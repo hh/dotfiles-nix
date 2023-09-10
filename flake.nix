077aa2326b9e9feec492314c65e5327bb8ed1dec
Hippie Hacker <hh@ii.coop>
2023-09-10 12:40:04 +0200

    FIrst time
    Co-Authored-By: Michael Hoang <enzime@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+      # fixes issue with OverWrite Protection
+      # Mock our nix.conf as over-writable
+      # This can be deleted after we run sudo nix run nix-darwin -- switch --flake ~/.config/nix-darwin
+      # and run the first switch
+      environment.etc."nix/nix.conf".knownSha256Hashes = [ "e00ec75a3579c55561a1a7569ee8f0174e80b6e1995facd0979c7048e4616f05" ];
+      # fixee
+      system.checks.verifyNixChannels = false;
+      security.pam.enableSudoTouchIdAuth = true;
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#m1
+    darwinConfigurations."m1" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."m1".pkgs;
+  };
+}
